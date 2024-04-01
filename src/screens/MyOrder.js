d0a4b3f1cbdbb079d7fc1c70c2db5ba2ecf3bf16
sakrana <164612801+sakrana@users.noreply.github.com>
2024-04-01 16:19:05 +0530

    Update MyOrder.js

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -8,7 +8,7 @@ export default function MyOrder() {
 
     const fetchMyOrder = async () => {
         console.log(localStorage.getItem('userEmail'))
-        await fetch("https://mernback-j808.onrender.com/api/myOrderData", {
+        await fetch("https://mernback-1-zoea.onrender.com/api/myOrderData", {
             // credentials: 'include',
             // Origin:"http://localhost:3000/login",
             method: 'POST',
